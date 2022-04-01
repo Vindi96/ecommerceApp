0595525baf220adee46323869346babfdfe5e0d9
Vindi96 <vindya.madhushani96@gmail.com>
2022-04-01 15:13:40 +0530

    develop image drawer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecommerceApp'`, () => {
+  it(`should have as title 'ecomerceApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecommerceApp');
+    expect(app.title).toEqual('ecomerceApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ecommerceApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ecomerceApp app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ImageCarouselComponent } from './components/image-carousel/image-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    CategoryListComponent,
+    NavigationComponent,
+    ImageCarouselComponent
   ],
   imports: [
     BrowserModule,
